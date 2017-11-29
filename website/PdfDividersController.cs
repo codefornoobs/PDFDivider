e28415740f5cf811af1aac9e9ac960487d11062e
Diogo Cró <diogocror@gmail.com>
2017-11-29 23:22:42 +0000

    added web API

---
+++ website/PdfDividersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace website
+{
+    [Route("api/[controller]")]
+    public class PdfDividersController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value2", "value1" };
+        }
+    }
+}
